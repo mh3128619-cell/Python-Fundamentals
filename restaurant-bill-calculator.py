bbbe2b150a2a80810a3d36c240c0c2b2c5c2eb49
Mohamed Hamed <mh3128619@gmail.com>
2026-07-04 04:02:53 +0300

    Add restaurant bill calculator script

---
+++ restaurant-bill-calculator.py
@@ -0,0 +1,6 @@
+meal_name=input("Enter the name of your meal:")
+meal_price=float(input("What is the price of the meal?"))
+drink_price=float(input("What is the price of the drink?"))
+total_price=meal_price+drink_price
+print("Thank you")
+print("Your total price is :"+str(total_price))
