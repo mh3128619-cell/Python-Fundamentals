6c5f992066a358b4f6e3fa89ccebe386971db699
Mohamed Hamed <mh3128619@gmail.com>
2026-07-04 07:36:16 +0300

    Add name analysis functionality in Name-analyzer.py

---
+++ Name-analyzer.py
@@ -0,0 +1,6 @@
+name=input("Enter your Full Name: ")
+space_index=name.index(" ")
+name_length=len(name)
+
+print("your first name ends at index :"+str(space_index))
+print("your last name starts at index :"+str(name_length))
