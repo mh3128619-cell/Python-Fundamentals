e4afebbdc0bd383ba023b9ed7799c8f505e9bad9
Mohamed Hamed <mh3128619@gmail.com>
2026-07-04 04:13:27 +0300

    Add Remember-Allah.py for problem-solving advice

---
+++ Remember-Allah.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+problem = input("Enter your problem: ")
+the_solution = "Remember Allah"
+
+print("\n--- Summary ---")
+print(name, "your problem is (", problem, ") and the solution is:", the_solution)
