18d77f5f43f67635122539e97ea7a0875df2c192
Mohamed Hamed <mh3128619@gmail.com>
2026-07-07 10:23:59 +0300

    Implement try limit for guessing game
    Added a try limit to the guessing game and updated messages.

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,13 +1,20 @@
 success_number = 7
 user_number = 0
+tries = 10
 
-while user_number != success_number:
+while user_number != success_number and tries > 0:
     user_number = int(input("Please, Enter the guessing number: "))
-    
+
     if user_number == success_number:
         print("You are right!")
         break
     else:
-        print("Invalid input, please try again.")
+        tries = tries - 1
+        if tries > 0:
+            print(f"Invalid input, please try again. You have {tries} tries left.")
+        else:
+            print("Game over! You ran out of tries.")
+
 else:
-    print("Thank you")
+    if user_number == success_number:
+        print("Thank you for playing!")
