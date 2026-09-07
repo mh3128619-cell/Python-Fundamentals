f4561440085b16d6485bec0966b2659f17280216
Mohamed Hamed <mh3128619@gmail.com>
2026-09-07 20:31:26 +0300

    Add tax calculation for product price

---
+++ Lambda_tax_calculator.py
@@ -0,0 +1,3 @@
+price_of_the_prouduct=int(input("Please,Enter the price of the product:"))
+price_after_tax=lambda price_of_the_prouduct:price_of_the_prouduct*1.15
+print(price_after_tax(price_of_the_prouduct))
