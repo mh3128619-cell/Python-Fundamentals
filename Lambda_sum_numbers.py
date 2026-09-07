78de2178b54a05bd25ecd45b28470ccdf061b8ce
Mohamed Hamed <mh3128619@gmail.com>
2026-09-07 20:30:54 +0300

    Create Lambda_sum_numbers.py

---
+++ Lambda_sum_numbers.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+num3=int(input("Enter the third number"))
+sum_of_nums=lambda num1,num2,num3:num1+num2+num3
+print(sum_of_nums(num1,num2,num3))
